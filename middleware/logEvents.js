a993485714a3707ce017f64904f38793a9f6027c
Huy Nguyen <huykst137@gmail.com>
2022-10-26 09:32:59 +0700

    Middleware

--- middleware/logEvents.js
+++ middleware/logEvents.js
@@ -11,16 +11,25 @@ const logEvents = async (message, logName) => {
   console.log(logItem);
 
   try {
-    if (!fs.existsSync(path.join(__dirname, "logs"))) {
-      await fs.promises.mkdir(path.join(__dirname, "logs"));
+    if (!fs.existsSync(path.join(__dirname, "..", "logs"))) {
+      await fs.promises.mkdir(path.join(__dirname, "..", "logs"));
     }
     // console.log();
     // testing
     // testing1
-    await fsPromises.appendFile(path.join(__dirname, "logs", logName), logItem);
+    await fsPromises.appendFile(
+      path.join(__dirname, "..", "logs", logName),
+      logItem
+    );
   } catch (err) {
     console.log(err);
   }
 };
 
-module.exports = logEvents;
+const logger = (req, res, next) => {
+  logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, "reqLog.txt");
+  console.log(`${req.method} ${req.path}`);
+  next();
+};
+
+module.exports = { logger, logEvents };
