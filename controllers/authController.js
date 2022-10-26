0c68a6f6516243cb35f31393f145c820b6df0e73
Huy Nguyen <huykst137@gmail.com>
2022-10-26 16:45:07 +0700

    JWT Auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,19 +6,52 @@ const usersDB = {
 };
 
 const bcrypt = require("bcrypt");
+const path = require("path");
+
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const fsPromises = require("fs").promises;
+
 const handleLogin = async (req, res) => {
   const { user, pwd } = req.body;
   if (!user || !pwd)
     return res
       .status(400)
       .json({ message: "Username and password are required." });
+
   const foundUser = usersDB.users.find((person) => person.username === user);
   if (!foundUser) return res.sendStatus(401); // Unauthorized
   //   evaluate password
   const match = await bcrypt.compare(pwd, foundUser.password);
   if (match) {
     // create JWTs
-    res.json({ success: `User ${user} is logged in!` });
+    const accessToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "30s" }
+    );
+    const refreshToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+    const othersUsers = usersDB.users.filter(
+      (person) => person.username !== foundUser.username
+    );
+    const currentUser = { ...foundUser, refreshToken };
+    usersDB.setUsers([...othersUsers, currentUser]);
+    await fsPromises.writeFile(
+      path.join(__dirname, "..", "model", "users.json"),
+      JSON.stringify(usersDB.users)
+    );
+
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    res.json({ accessToken });
   } else {
     res.sendStatus(401);
   }
